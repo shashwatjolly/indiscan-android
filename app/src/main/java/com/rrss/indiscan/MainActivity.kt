93092474f77c6ade9d32f5fc4e8fe2a3284e58b8
Shashwat Jolly <shashwat.jolly@gmail.com>
2020-07-11 13:39:36 +0530

    Initial commit

---
+++ app/src/main/java/com/rrss/indiscan/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.rrss.indiscan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var appBarConfiguration: AppBarConfiguration? = null
+    private var drawerLayout: DrawerLayout? = null
+    private var navController: NavController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar) //set the toolbar
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.scanFragment,
+                R.id.processFragment
+            ), drawerLayout
+        )
+
+        setupActionBarWithNavController(navController!!, appBarConfiguration!!) //the most important part
+        navView.setupWithNavController(navController!!) //the second most important part
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawers_close)
+        drawerLayout?.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration!!) || super.onSupportNavigateUp()
+    }
+}
